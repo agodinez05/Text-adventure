08aef5c7527341633a94ded660a1b6c23eebb19e
agodinez05 <90656201+agodinez05@users.noreply.github.com>
2021-10-15 12:05:26 -0700

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,8 @@
+def start ():
+    #give some prompts
+    print("It is dark and you are in a rattling box, moving upward. As you approach the light you see a group of people crowding around you. You see the maze. Tall walls of rock.")
+    print ("Now you must lead the group to safety but be careful of the grievers, flesh eating robots.")
+
+    #convert the player's input() to lower_case
+    answer = input(">").lower()
+
